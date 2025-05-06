4e2f4593b98aa28b042e541a1f39b4cf94aa391a
yashpola <yashwit.po@gmail.com>
2025-05-06 14:17:29 +0800

    Add fastApi endpoint for external communication

--- main.py
+++ main.py
@@ -1,15 +1,59 @@
-from typing import Optional
-
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import JSONResponse
+from pydantic import BaseModel
+
+import json
+
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+FRONTEND_DEV_URL = os.getenv("FRONTEND_DEV_URL")
+FRONTEND_PROD_URL = os.getenv("FRONTEND_PROD_URL")
+
+import sys
+
+sys.path.insert(0, "/Users/yashpola/Repositories/AlgoRepo/algorithms/flow/")
+
+from maxflow import max_flow
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[FRONTEND_DEV_URL, FRONTEND_PROD_URL],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+class RequestData(BaseModel):
+    graph: str
+    src: str
+    sink: str
+
+
+class MaxFlowResult(BaseModel):
+    flowVal: str
 
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "test"}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+def stog(s: str) -> list[list[int]]:
+    graph = []
+    for row in s:
+        graph.append(s[row])
+    return graph
+
+
+@app.post("/", response_model=MaxFlowResult)
+def root(data: RequestData) -> MaxFlowResult:
+    print(data.graph, data.src, data.sink)
+    res = max_flow(stog(json.loads(data.graph)), int(data.src), int(data.sink))[0]
+    return dict(flowVal=str(res))
